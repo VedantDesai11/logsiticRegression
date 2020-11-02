9ba5ab297796f0059873a612a6faedaf8802b5c5
vedantdesai11 <vedantpratikdesai@gmail.com>
2020-11-01 20:14:02 -0600

    Completed Assignment2

--- ML_A2_P2.py
+++ ML_A2_P2.py
@@ -2,15 +2,15 @@ import idx2numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from copy import deepcopy
+from tqdm import tqdm
 
-def cross_entropy(p, q):
-	return -sum([p[i]*math.log(q[i]) for i in range(len(p))])
+
+def cross_entropy(q, p):
+    return -sum([p[j] * math.log(q[j]) for j in range(len(p))])
 
 
 def softmax(x):
     return np.exp(x) / np.sum(np.exp(x), axis=0)
-
 
 class Model:
     def __init__(self, lr=0.01, batch_size=32, tolerance=0.0001):
@@ -21,108 +21,128 @@ class Model:
         self.iterations = 0
         self.actual = None
 
-    def train(self, train, label):
-
-        # number of different classes to classify. 5 in our case
-        classes = len(np.unique(label))
+    def train(self, train, label, classes=5):
 
         batch_size = self.batch_size
+        train_size = train.shape[0]
 
         # training indices from 0 - len(train_set)
-        train_idx = np.arange(len(label))
+        train_idx = np.arange(train_size)
 
         # shuffle training indices
         np.random.shuffle(train_idx)
 
         # create batches of given batch size from shuffled indices
-        batches = np.array_split(train_idx, len(label)//batch_size+1)
+        batches = np.array_split(train_idx, train_size // batch_size + 1)
 
         # Initialize weight matrix, bias and set batch_size
-        W = np.random.random((28*28, classes))
+        W = np.random.random((classes, 28*28)) * 0.01
         bias = 1
+        loss_list = []
 
-        for i in range(10000):
+        for i in tqdm(range(10000)):
             for batch in batches:
+                X = train[batch]
+                y = label[batch]
 
-                X = np.zeros((batch_size, 28*28))
-                for j in range(len(train[batch])):
-                    X[j] = train[batch][j].flatten()
+                z = np.dot(X, W.T) + bias  # Shape = (32,5)
 
-                l = label[batch]
-                y = np.zeros((batch_size, classes))  # Shape = (32,5)
+                h = np.zeros((len(batch), classes))
 
-                for j in range(len(y)):
-                    y[j][l[j]] = 1
+                for j,val in enumerate(z):
+                    h[j] = softmax(val)
 
-                z = np.dot(X, W) + bias # Shape = (32,5)
-                h = softmax(z) # Shape = (32,5)
-                dz = h - y # Shape = (32,5)
-                dW = np.dot(X.T, dz) / batch_size # Shape = (32,764)
-                db = np.sum(dz) / batch_size
+                dz = h - y  # Shape = (32,5)
+                dW = np.dot(dz.T, X) / len(batch)  # Shape = (5,764)
+                db = np.sum(dz)/len(batch)
 
-                if i % 1000 == 0:
-                    print(h[-1], y[-1])
-                    loss = cross_entropy(h[-1].reshape(classes,1), y[-1].reshape(classes,1))
-                    print(f"Loss at iteration({i} = {loss})")
+            loss = cross_entropy(h[-1].reshape(classes, 1), y[-1].reshape(classes, 1))[0]
+            loss_list.append(loss)
 
-                W = W - self.learning_rate * dW
-                bias -= self.learning_rate * db
+            if loss < 0.05:
+                print(f"Performing Early Stopping.\nLoss at iteration {i} = {loss}")
+                break
+
+            if i % 500 == 0:
+                print(f"\nLoss at iteration {i} = {loss}")
+
+            W = W - self.learning_rate * dW
+            bias = bias - self.learning_rate * db
 
         self.W = W
         self.bias = bias
-
+        self.losslist = loss_list
 
     def predict(self, test, l):
 
         # number of different classes to classify. 5 in our case
         classes = len(np.unique(l))
         testsize = test.shape[0]
+        actual = l.reshape((testsize, 1))
 
         # get trained weights
         W = self.W
         bias = self.bias
 
-        X = test.reshape((testsize, 28 * 28))  # Shape = (test_len,764)
+        X = np.array([X.flatten() for X in test])
 
-        z = np.dot(X, W) + bias
-        h = softmax(z)
+        z = np.dot(X, W.T) + bias
+        predictions = np.zeros((testsize, 1))
+        correct = 0
 
-        predictions = np.zeros((testsize,1))
+        for i, val in enumerate(z):
+            prediction = np.argmax(softmax(val))
+            predictions[i] = prediction
+            if prediction == actual[i]:
+                correct += 1
 
-        for i in range(len(h)):
-            predictions[i] = np.argmax(h[i])
-
-        accuracy = (np.count_nonzero(np.equal(l, predictions))/testsize) * 100
-        print(f'Accuracy with LR({self.learning_rate}) = {accuracy}%')
+        accuracy = (correct/testsize) * 100
+        print(f'Accuracy = {accuracy}%')
 
         return predictions
 
 
-    def confusionMatrix(self, actual, predicted):
-        TP = 0
-        FP = 0
-        TN = 0
-        FN = 0
+    def precision_Recall(self, actual, predicted):
+
+        PrecisionList = []
+        RecallList = []
+
+        confusionMatrix = np.zeros((5,5))
 
         for i in range(len(predicted)):
-            if actual[i] == predicted[i] == 1:
-                TP += 1
-            if predicted[i] == 1 and actual[i] != predicted[i]:
-                FP += 1
-            if actual[i] == predicted[i] == 0:
-                TN += 1
-            if predicted[i] == 0 and actual[i] != predicted[i]:
-                FN += 1
+            pred = int(predicted[i])
+            a = actual[i]
 
-        return TP, FP, TN, FN
+            if pred == a:
+                confusionMatrix[pred][pred] += 1
+            else:
+                confusionMatrix[pred][a] += 1
+
+        for c in np.unique(actual):
+            TP = confusionMatrix[c][c]
+            TN = 0
+            FP = 0
+            FN = 0
+            for row in np.unique(actual):
+                for column in np.unique(actual):
+                    if column != c and row != c:
+                        TN += confusionMatrix[row][column]
+                    if row == c and column != c:
+                        FP += confusionMatrix[row][column]
+                    if row != c and column == c:
+                        FN += confusionMatrix[row][column]
+
+            RecallList.append(TP/(TP+FN))
+            PrecisionList.append(TP/(TP+FP))
+
+        return confusionMatrix, PrecisionList, RecallList
 
 
 
 if __name__ == "__main__":
-
-    batchsize = 32
+    batchsize = 200
     tolerance = 0.0001
-    learningRate = 0.001
+    learningRate = 0.01
 
     X_train, X_test, y_train, y_test = idx2numpy.convert_from_file('mnist/train-images-idx3-ubyte'), \
                                        idx2numpy.convert_from_file('mnist/t10k-images-idx3-ubyte'), \
@@ -132,19 +152,38 @@ if __name__ == "__main__":
     train_subset_idx = np.where(y_train < 5)[0]
     test_subset_idx = np.where(y_test < 5)[0]
 
-    X_train, y_train, X_test, y_test = X_train[train_subset_idx], y_train[train_subset_idx], X_test[test_subset_idx], y_test[test_subset_idx]
+    X_train, y_train, X_test, y_test = X_train[train_subset_idx], y_train[train_subset_idx], X_test[test_subset_idx], \
+                                       y_test[test_subset_idx]
 
     # Normalize
     X_train = X_train / 255.0
     X_test = X_test / 255.0
 
+    # flatten image data from 28x28 to 784,1
+    X_train = np.array([X.flatten() for X in X_train])
 
+    print(f'X_train.shape={X_train.shape}' )
+
+    # one hot encoding for labels
+    one_hot_y_train = np.zeros((len(y_train), len(np.unique(y_train))))  # Shape = (32,5)
+
+    for i in range(len(one_hot_y_train)):
+        one_hot_y_train[i][y_train[i]] = 1
+
+    print(f'one_hot_y_train.shape={one_hot_y_train.shape}')
 
     model = Model(learningRate, batchsize, tolerance)
-    model.train(X_train, y_train)
-    predictions = model.predict(X_test, y_test)
-    print(predictions)
+    model.train(X_train, one_hot_y_train)
 
+    predictions = model.predict(X_test, y_test)
+    cm, precision, recall = model.precision_Recall(y_test, predictions)
+
+    print(cm)
+    for i, c in enumerate(np.unique(y_test)):
+        print(f'For Class {c}, Precision = {precision[i]}, Recall = {recall[i]}')
+
+    plt.plot(range(len(model.losslist)), model.losslist)
+    plt.show()
 
 
 
